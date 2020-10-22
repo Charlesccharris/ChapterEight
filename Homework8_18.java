0224bd864063b2e22c8281be3bfee422c46c4e45
Charlesccharris <harriscc@student.swosu.edu>
2020-10-22 15:38:38 -0500

    Added the insructions for the homework

---
+++ Homework8_18.java
@@ -0,0 +1,15 @@
+class Homework8_18{
+	public static void main(String[] args){
+		/*(Shuffle rows)
+		Write a method that shuffles the rows in a two-dimensional
+		int array using the following header:
+
+		public static void shuffle(int[][] m)
+
+		Write a test program that shuffles the following matrix:
+		int[][] m = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};*/
+
+		System.out.println("Hello");
+
+	}
+}
