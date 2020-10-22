a9b7af556dcc12b5d73123999c84fc7868859619
Charlesccharris <harriscc@student.swosu.edu>
2020-10-22 15:37:39 -0500

    Made sure the code works, and added the requirements

---
+++ Homework8_3.java
@@ -0,0 +1,8 @@
+class Homework8_3 {
+	public static void main(String[] args){
+		/*8.3 (Sort students on grades)
+		Rewrite Listing 8.2, GradeExam.java, to display the students in increasing order of the number of correct answers.*/
+
+		System.out.println("HEllO");
+	}
+}
