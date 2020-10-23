d57d55a35af6cdbcd39fc9e1785e9219f2082f15
Charlesccharris <harriscc@student.swosu.edu>
2020-10-22 23:18:38 -0500

    Got it to work, and check to see if anyone won a round, and if they did it stops the game and says who won

--- Homework8_9.java
+++ Homework8_9.java
@@ -8,16 +8,16 @@ class Homework8_9{
 		A draw (no winner) occurs when all the cells on the grid have been filled with tokens and neither player has achieved a win.
 		Create a program for playing tic-tac-toe. The program prompts two players to enter an X token and O token alternately.
 		Whenever a token is entered, the program redisplays the board on the console and determines the status of the game (win, draw, or continue).*/
-		System.out.println("Start Program");
+		System.out.println("Start Tic-Tac-Toe");
 
 		Scanner input = new Scanner(System.in);
 
 		char[][] board = makeBoard();
 		boolean keepGoing = true;
 
-		System.out.print("Hello player one!");
+		System.out.print("Hello player one! ");
 		String playerOne = getName(input);
-		System.out.print("Hello player two!");
+		System.out.print("Hello player two! ");
 		String playerTwo = getName(input);
 		while(keepGoing){
 			board = makeBoard();
@@ -45,7 +45,7 @@ class Homework8_9{
 				System.out.print(board[i][j]);
 			}
 		}
-		System.out.println("");
+		System.out.println("\n");
 
 	}
 
@@ -56,45 +56,48 @@ class Homework8_9{
 	}
 
 	public static boolean playTicTacToe(char[][] board, Scanner input, String playerOne, String playerTwo){
+		boolean noWin = true;
 		int turns = 0;
-		while(turns < 9){
+		while(turns < 9 && noWin){
 			if(turns%2 == 0){
-				getPlayerOnePlacement(board, input);
+				getPlayerOnePlacement(board, input, playerOne);
 				displayBoard(board);
 				turns++;
 			}
 			else{
-				getPlayerTwoPlacement(board, input);
+				getPlayerTwoPlacement(board, input, playerTwo);
 				displayBoard(board);
 				turns++;
 			}
 			if(turns >= 5){
-				checkForWin(board, playerOne, playerTwo);
-				System.out.println("Hello");
+				noWin = checkForWin(board, playerOne, playerTwo);
 			}
+		}
+		if(noWin){
+			System.out.println("Looks like no one won.");
 		}
 		boolean rematch = askForRematch(input);
 		return rematch;
 	}
 
-	public static void getPlayerOnePlacement(char[][] board, Scanner input){
+	public static void getPlayerOnePlacement(char[][] board, Scanner input, String player){
 		int choice = 0;
 		boolean invalid = true;
 		char piece = 'X';
 		while(invalid){
-			System.out.print("Enter the square you would like to put an X in: ");
+			System.out.print(player + " please enter the square you would like to put an X in: ");
 			choice = input.nextInt();
 			invalid = checkValidInput(board, choice);
 		}
 		changeBoard(board, choice, piece);
 	}
 
-	public static void getPlayerTwoPlacement(char[][] board, Scanner input){
+	public static void getPlayerTwoPlacement(char[][] board, Scanner input, String player){
 		int choice = 0;
 		boolean invalid = true;
 		char piece = 'O';
 		while(invalid){
-			System.out.print("Enter the square you would like to put an O in: ");
+			System.out.print(player + " please enter the square you would like to put an O in: ");
 			choice = input.nextInt();
 			invalid = checkValidInput(board, choice);
 		}
@@ -185,36 +188,28 @@ class Homework8_9{
 	public static void changeBoard(char[][] board, int choice, char piece){
 		switch(choice){
 			case 1:
-				board[0][1] = piece;
-				break;
+				board[0][1] = piece;break;
 			case 2:
-				board[0][5] = piece;
-				break;
+				board[0][5] = piece;break;
 			case 3:
-				board[0][9] = piece;
-				break;
+				board[0][9] = piece;break;
 			case 4:
-				board[2][1] = piece;
-				break;
+				board[2][1] = piece;break;
 			case 5:
-				board[2][5] = piece;
-				break;
+				board[2][5] = piece;break;
 			case 6:
-				board[2][9] = piece;
-				break;
+				board[2][9] = piece;break;
 			case 7:
-				board[4][1] = piece;
-				break;
+				board[4][1] = piece;break;
 			case 8:
-				board[4][5] = piece;
-				break;
+				board[4][5] = piece;break;
 			case 9:
 				board[4][9] = piece;
 		}
 	}
 
 	public static boolean askForRematch(Scanner input){
-		System.out.print("Would you like a rematch?\ny for yes, and n for no");
+		System.out.print("Would you like a rematch?\ny for yes, and n for no: ");
 		char answer = input.next().charAt(0);
 		if(answer == 'y' || answer == 'Y'){
 			return true;
@@ -224,66 +219,52 @@ class Homework8_9{
 		}
 	}
 
-	public static void checkForWin(char[][] board, String p1, String p2){
+	public static boolean checkForWin(char[][] board, String p1, String p2){
 		String winner = "";
 		int xOrO = 0;
 		boolean winnerYes = false;
-		char[] xWin = {'X', 'X', 'X'};
-		char[] oWin = {'O', 'O', 'O'};
 
 		char[][] winningConditions ={
-		char[] topRow = {board[0][1], board[0][5], board[0][9]};
-		char[] middleRow = {board[2][1], board[2][5], board[2][9]};
-		char[] bottomRow = {board[4][1], board[4][5], board[4][9]};
-		char[] leftColumn = {board[0][1], board[2][1], board[4][1]};
-		char[] middleColumn = {board[0][5], board[2][5], board[4][5]};
-		char[] rightColumn = {board[0][9], board[2][9], board[4][9]};
-		char[] leftDiagnal = {board[0][1], board[2][5], board[4][9]};
-		char[] rightDiagnal = {board[0][9], board[2][5], board[4][1]};
+			{board[0][1], board[0][5], board[0][9]},
+			{board[2][1], board[2][5], board[2][9]},
+			{board[4][1], board[4][5], board[4][9]},
+			{board[0][1], board[2][1], board[4][1]},
+			{board[0][5], board[2][5], board[4][5]},
+			{board[0][9], board[2][9], board[4][9]},
+			{board[0][1], board[2][5], board[4][9]},
+			{board[0][9], board[2][5], board[4][1]}
+		};
 
-		for(i = 0; i < 2; i++){
-			if(topRow[i] != xWin[i])
-			
+		for(int i = 0; i < winningConditions.length; i++){
+			char one = winningConditions[i][0];
+			char two = winningConditions[i][1];
+			char three = winningConditions[i][2];
+			if(one == 'X' && two == 'X' && three == 'X'){
+				winnerYes = true;
+				xOrO = 1;
 				break;
-			else if(
+			}
+			else if(one == 'O' && two == 'O' && three =='O'){
+				winnerYes = true;
+				xOrO = 2;
+				break;
+			}
+			else{
+				winnerYes = false;
+			}
 		}
-		if(JSON.stringify(topRow) == JSON.stringify(xWin)){
-			winner = p1;
-			winnerYes = true;
-		}
-		else if(topRow == oWin){
-			winner = p2;
-			winnerYes = true;
-		}
-/*		else if(middleRow == xWin
-		else if(middleRow == oWin
-		else if(bottomRow == xWin
-		else if(bottomRow == oWin
-		else if(leftColumn == xWin
-		else if(leftColumn == oWin
-		else if(middleColumn == xWin
-		else if(middleColumn == oWin
-		else if(rightColumn == xWin
-		else if(rightColumn == oWin
-		else if(leftDiagonal == xWin
-		else if(leftDiagonal == oWin
-		else if(rightDiagonal == xWin
-		else if(rightDiagonal == oWin
-		else
-*/
+
 		if(xOrO == 1){
 			winner = p1;
-			winnerYes = true;
-		else if(xOrO  ==2){
-
 		}
+		else if(xOrO == 2){
+			winner = p2;
+		}
+
 		if(winnerYes){
 			System.out.println(winner + " won the round!");
+			return false;
 		}
-	}
-	public static void display(char[] array){
-		for(int i = 0; i < array.length; i++){
-			System.out.print(array[i]);
-		}
+		return true;
 	}
 }
